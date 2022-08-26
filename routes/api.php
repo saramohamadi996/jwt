b66d97a2f887782ada603b3207f950ee1cf34427
sara <mohamadisa996@gmail.com>
2022-08-26 23:26:25 +0430

    dddsf

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+    Route::post('refresh', 'refresh');
 
 });
 
